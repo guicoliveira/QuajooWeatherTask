76513375b29582619870bb941823417a12e810b1
guicoliveira <gc96oliveira@gmail.com>
2022-01-27 22:16:58 +0000

    initial commit

---
+++ docker-entrypoint-initdb.d/mongo-init.js
@@ -0,0 +1,35 @@
+db.createCollection('cities');
+
+db.cities.insertMany([
+    {
+        name: 'London'
+    },
+    {
+        name: 'Paris'
+    },
+    {
+        name: 'Lisbon'
+    },
+    {
+        name: 'Rome'
+    },
+    {
+        name: 'Milan'
+    },
+    {
+        name: 'Porto'
+    },
+    {
+        name: 'Leipzig'
+    },
+    {
+        name: 'Aveiro'
+    },
+    {
+        name: 'Frankfurt'
+    },
+    {
+        name: 'Amsterdam'
+    },
+         
+]);
